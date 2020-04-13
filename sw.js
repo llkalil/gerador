e149b1fa1be704a3fdbc501a74d2a491768ca27c
llkalil <62946771+llkalil@users.noreply.github.com>
2020-04-13 20:59:44 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -37,3 +37,18 @@ self.addEventListener('fetch', function (event) {
     })
   );
 });
+
+function showNotification() {
+  Notification.requestPermission(function(result) {
+    if (result === 'granted') {
+      navigator.serviceWorker.ready.then(function(registration) {
+        registration.showNotification('Vibration Sample', {
+          body: 'Buzz! Buzz!',
+          icon: '../images/touch/chrome-touch-icon-192x192.png',
+          vibrate: [200, 100, 200, 100, 200, 100, 200],
+          tag: 'vibration-sample'
+        });
+      });
+    }
+  });
+}
